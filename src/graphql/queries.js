c9eeb4df7f48db2044826229a986e2b0cb1db868
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-05-12 17:31:25 +0200

    Add GraphQL API

---
+++ src/graphql/queries.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDocument = /* GraphQL */ `
+  query GetDocument($id: ID!) {
+    getDocument(id: $id) {
+      id
+      name
+      abstract
+      type
+      issueDate
+      file
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listDocuments = /* GraphQL */ `
+  query ListDocuments(
+    $filter: ModelDocumentFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDocuments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        abstract
+        type
+        issueDate
+        file
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateDocument = /* GraphQL */ `
+  subscription OnCreateDocument {
+    onCreateDocument {
+      id
+      name
+      abstract
+      type
+      issueDate
+      file
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateDocument = /* GraphQL */ `
+  subscription OnUpdateDocument {
+    onUpdateDocument {
+      id
+      name
+      abstract
+      type
+      issueDate
+      file
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteDocument = /* GraphQL */ `
+  subscription OnDeleteDocument {
+    onDeleteDocument {
+      id
+      name
+      abstract
+      type
+      issueDate
+      file
+      createdAt
+      updatedAt
+    }
+  }
+`;
