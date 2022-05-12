bb11829d05daa84f5190e2e2980a495ff4dafef2
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-05-12 14:36:14 +0200

    Re initialize react app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
-import 'mdbreact/dist/css/mdb.css';
 import App from './App';
-import AppRouter from './routers/AppRouter'
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-// import './App.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
