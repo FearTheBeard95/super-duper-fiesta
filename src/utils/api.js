284ba71f223c14e50d55a2e2898adb05e832742b
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-05-17 23:09:47 +0200

    Add Documents API's Handlers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,6 @@
+import * as queries from '../graphql/queries';
 import { Storage, API, graphqlOperation } from 'aws-amplify';
 import * as mutations from '../graphql/mutations';
-import * as queries from '../graphql/queries';
-import * as subscriptions from '../graphql/subscriptions';
-
 export async function createDocument(document) {
   await Storage.put(document.name, document.file);
   const file = await Storage.get(document.name);
@@ -10,6 +8,40 @@ export async function createDocument(document) {
   const data = await API.graphql(
     graphqlOperation(mutations.createDocument, { input: { ...document, file } })
   );
+
+  return data;
+}
+
+export async function deleteDocument(id) {
+  await Storage.remove(document.name);
+  const data = await API.graphql(
+    graphqlOperation(mutations.deleteDocument, { input: { id } })
+  );
+
+  return data;
+}
+
+// export async function updateDocument(id) {
+//   await Storage.put(document.name, document.file);
+//   const file = await Storage.get(document.name);
+
+//   const data = await API.graphql(graphqlOperation(mutations.updateDocument), {
+//     input: { id },
+//   });
+
+//   return data;
+// }
+
+export async function getAllDocuments() {
+  const data = await API.graphql(graphqlOperation(queries.listDocuments));
+
+  return data;
+}
+
+export async function getDocument(id) {
+  const data = await API.graphql(graphqlOperation(queries.getDocument), {
+    input: id,
+  });
 
   return data;
 }
